52d819064d72a69d20c103588e781df83b02b23c
xavier-kong <kongxianchengxavier@gmail.com>
2021-11-30 17:44:43 +0800

    finished module 4

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+// Include the AWS SDK module
+const AWS = require('aws-sdk');
+// Instantiate a DynamoDB document client with the SDK
+let dynamodb = new AWS.DynamoDB.DocumentClient();
+// Use built-in module to get current date & time
+let date = new Date();
+// Store date and time in human-readable format in a variable
+let now = date.toISOString();
 // Define handler function, the entry point to our code for the Lambda service
 // We receive the object that triggers the function as a parameter
 exports.handler = async (event) => {
     // Extract values from event and format as strings
     let name = JSON.stringify(`Hello from Lambda, ${event.firstName} ${event.lastName}`);
+    // Create JSON object with parameters for DynamoDB and store in a variable
+    let params = {
+        TableName:'HelloWorldDatabase',
+        Item: {
+            'ID': name,
+            'LatestGreetingTime': now
+        }
+    };
+    // Using await, make sure object writes to DynamoDB table before continuing execution
+    await dynamodb.put(params).promise();
     // Create a JSON object with our response and store it in a constant
     const response = {
         statusCode: 200,
