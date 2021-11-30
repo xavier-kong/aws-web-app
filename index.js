eb5d04b94211e67763a448e0b10a41609f03c417
xavier-kong <kongxianchengxavier@gmail.com>
2021-11-30 17:21:11 +0800

    finished module 2

---
+++ index.js
@@ -0,0 +1,13 @@
+// Define handler function, the entry point to our code for the Lambda service
+// We receive the object that triggers the function as a parameter
+exports.handler = async (event) => {
+    // Extract values from event and format as strings
+    let name = JSON.stringify(`Hello from Lambda, ${event.firstName} ${event.lastName}`);
+    // Create a JSON object with our response and store it in a constant
+    const response = {
+        statusCode: 200,
+        body: name
+    };
+    // Return the response constant
+    return response;
+};
